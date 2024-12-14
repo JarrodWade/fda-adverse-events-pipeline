e13c5f8134fd744585845a703b41f397a0110c60
JarrodWade <jarrod.wadej@gmail.com>
2024-12-14 11:19:50 -0700

    simplify staging approach, moving primary transformation logic to master table, per dbt's best practices for layered architecture

--- dags/dbt/models/staging/stg_adverse_events.sql
+++ dags/dbt/models/staging/stg_adverse_events.sql
@@ -1,3 +1,4 @@
+-- models/staging/stg_adverse_events.sql
 {{ config(materialized='view') }}
 
 SELECT
@@ -10,17 +11,13 @@ SELECT
     qualification as qualification,
     reactionmeddrapt as reaction_meddra_pt,
     reactionoutcome as reaction_outcome_cd,
-    {{ map_outcome('reaction_outcome_cd') }} AS mapped_outcome, -- map outcome from code
     receiptdate as receipt_date,
     receivedate as receive_date,
     transmissiondate as transmission_date,
     patientonsetage as patient_age,
     patientonsetageunit as patient_age_unit,
-    {{ calculate_age_in_years('patient_age', 'patient_age_unit') }} AS calculated_age_in_years, -- calculate age in years
-    patientweight as patient_weight_kg, -- currently in kg
+    patientweight as patient_weight_kg,
     patientsex as patient_sex,
-    {{ determine_age_group('calculated_age_in_years') }} AS age_group, --determine age group
-    {{ kg_to_lbs('patient_weight_kg') }} AS patient_weight_lbs, -- convert kg to lbs
     serious as is_serious,
     seriousnessdeath as seriousness_death,
     seriousnesshospitalization as seriousness_hospitalization,
@@ -32,5 +29,5 @@ SELECT
     primarysourcecountry as primary_source_country,
     fulfillexpeditecriteria as fullfill_expedite_criteria,
     reporttype as report_type,
-    datepulled as date_pulled --timestamp
+    datepulled as date_pulled
 FROM {{ source('raw', 'ADVERSE_EVENTS_RAW') }}
